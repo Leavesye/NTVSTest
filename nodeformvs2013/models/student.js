0099a6c36a0e84edc4a17b1729b3706e505d687f
Leavesye <554963994@qq.com>
2016-05-31 15:58:17 +0800

    NTVSTest

---
+++ nodeformvs2013/models/student.js
@@ -0,0 +1,26 @@
+﻿var mongoose = require('mongoose');
+var StudentSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    gender: String,
+    birthday:Date
+});
+
+// 静态方法
+StudentSchema.statics = {
+    fetch: function (cb) { 
+        return this
+            .find({})
+            .exec(cb);
+    },
+    findById: function (id,cb) { 
+        return this.findone({_id:id}).exec(cb)
+    },
+    delete: function (id, cb) { 
+        return this.remove({ _id: id }).exec(cb);
+    }
+};
+
+var Student = mongoose.model('Student', StudentSchema);
+
+module.exports = Student;
---
+++ nodeformvs2013/routes/students.js
@@ -0,0 +1,29 @@
+﻿var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Student = require('../models/student')
+
+
+
+/* GET users listing. */
+router.get('/create', function (req, res) {
+    var student = new Student({ name: 'ys', age: 12, gender: '男', birthday: new Date() });
+    student.save();
+    res.send('respond with a resource');
+});
+
+router.get('/list', function (req, res) {
+    //Student.fetch(function (err, students) {
+    //    if (err)
+    //        console.log(error);
+    //    console.log(students);
+    //});
+    Student.remove({ name: 'ys' }, function (err) {
+        if (err) { console.log(err) }
+
+        console.log('%s删除成功','文档');
+    })
+    res.send('respond with a resource');
+});
+
+module.exports = router;
